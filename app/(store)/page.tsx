7b1db0feb1640b337ebf2e489c40d004c9c5f88d
Govind Raj Gupta <gg7078417058@gmail.com>
2025-01-21 13:05:35 +0530

    basic done

---
+++ app/(store)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Hello World 123</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
